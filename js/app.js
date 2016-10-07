e98d02d69169a48acd43f6aa947980ff55a9fd2e
Bert Ciccone <bertfciccone@gmail.com>
2016-10-06 19:11:37 -0700

    fix: fix the long pauses by moving counter to player object

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 //
+// Global variables
+//
+var numRows = 6;
+var numCols = 5;
+var colWidth = 101;
+var rowHeight = 83;
+
+// Character state transitions
+var moving = "moving";
+var crashing = "crashing";
+var splashing = "splashing";
+
+//
 // Shape - superclass
 //
 function Shape() {
@@ -17,8 +30,6 @@ function Shape() {
 
 // Move shape to col, row and update canvas coordinates
 Shape.prototype.move = function (col, row) {
-  var colWidth = 101;
-  var rowHeight = 83;
   if (col !== undefined) {
     this.col = col;
   };
@@ -42,12 +53,8 @@ function Enemy() {
   // we've provided one for you to get started
   // The image/sprite for our enemies, this uses
   // a helper we've provided to easily load images
-  this.sprite = 'images/enemy-bug.png';
-  this.rowOffset = -20;
-  // Enemy speed will range from 0.5 to 2.0 before dt factor
-  this.speed = 0.5 + Math.random() * 1.5;
+  this.setMoving();
 }
-
 // Enemy subclass extends Shape superclass
 Enemy.prototype = Object.create(Shape.prototype);
 Enemy.prototype.constructor = Enemy;
@@ -56,50 +63,40 @@ Enemy.prototype.constructor = Enemy;
 // Methods of Enemy subclass
 //
 
+// Set the "moving" state for an enemy
+Enemy.prototype.setMoving = function () {
+  this.state = moving;
+  this.sprite = 'images/enemy-bug.png';
+  this.rowOffset = -20;
+  // Enemy speed will range from 0.5 to 2.0 before dt factor
+  this.speed = 0.5 + Math.random() * 1.5;
+};
+
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function (dt) {
   // You should multiply any movement by the dt parameter
   // which will ensure the game runs at the same speed for
   // all computers.
-  var numCols = 5;
+
   // Update col position; after moving off the row, cycle back
   var newCol = this.col + this.speed * dt;
   if (newCol >= numCols) {
     newCol = -1;
   };
+
+  // Update position on the canvas
   this.move(newCol);
-  // console.info('Enemy moved to square ', this.col, ', ', this.row);
-  // Check for collision
-  if (player.row === this.row) {
-    if (Math.abs(player.col - this.col) < 0.8) {
-      if (player.state === moving) {
-        // Player has just crashed; start crashing
-        player.state = crashing;
-        player.iPause = player.maxPause;
-        player.sprite = 'images/crash.png';
-        player.rowOffset = 50;
-        player.move();
-        console.info('Player crashed ', player.col, ', ', player.row);
-      } else if (player.iPause > 0) {
-        // Keep pausing
-        player.iPause--;
-      } else if (player.iPause <= 0) {
-        // Done crashing; reset player image and restart
-        player.state = moving;
-        player.iPause = 0;
-        player.sprite = 'images/char-boy.png';
-        player.rowOffset = -10;
-        player.move(2, 5);
-        // console.info('Player moved to square ', this.col, ', ', this.row);
-      }
-    };
+
+  // Check for collision with player
+  if (player.state === moving &&
+    player.row === this.row &&
+    Math.abs(player.col - this.col) < 0.7) {
+    // Player has just crashed; start crashing
+    player.setCrashing();
+    player.move();
   };
 };
-
-var moving = "moving";
-var crashing = "crashing";
-var splashing = "splashing";
 
 // Player - subclass of Shape
 // Player must avoid the enemies and reach the water
@@ -107,14 +104,7 @@ function Player() {
   Shape.call(this); // call superclass constructor.
   // Variables applied to each of our instances go here,
   // we've provided one for you to get started
-  // The image/sprite for our layer, this uses
-  // a helper we've provided to easily load images
-  this.sprite = 'images/char-boy.png';
-  this.rowOffset = -10;
-  // When player state is "crashing" or "splashing", change image and pause for maxPause ticks
-  this.state = moving;
-  this.maxPause = 60;
-  this.iPause = 0;
+  this.setMoving();
 }
 // Player subclass extends Shape superclass
 Player.prototype = Object.create(Shape.prototype);
@@ -127,70 +117,91 @@ Player.prototype.constructor = Player;
 // Update the player's position, required method for game
 // Parameter: dt, a time delta between ticks
 Player.prototype.update = function (dt) {
-  // dt is not needed for player; action is driven by user input
   // Check for win when player is at row 0 at the water
-  if (this.row <= 0) {
-    if (this.state === moving) {
+  if (this.state === moving) {
+    if (this.row <= 0) {
       // Player has just won; start splashing
-      this.state = splashing;
-      this.iPause = this.maxPause;
-      this.sprite = 'images/splash.png';
-      this.rowOffset = 60;
+      this.setSplashing();
       this.move();
-    } else if (this.iPause > 0) {
-      // Keep pausing
-      this.iPause--;
-    } else if (this.iPause <= 0) {
-      // Done pausing; reset player image and restart
-      this.state = moving;
-      this.iPause = 0;
-      this.sprite = 'images/char-boy.png';
-      this.rowOffset = -10;
-      this.move(2, 5);
-      // console.info('Player moved to square ', this.col, ', ', this.row);
     }
+  } else {
+    // Player state is crashing or splashing; pause and restart
+    this.pauseAndRestart(dt);
+  }
+};
+
+Player.prototype.setMoving = function () {
+  // When player state is "moving", respond to player input
+  this.state = moving;
+  this.maxPause = 5;
+  this.iPause = 0;
+  this.sprite = 'images/char-boy.png';
+  this.rowOffset = -10;
+};
+
+Player.prototype.setCrashing = function () {
+  // When player state is "crashing", change image and pause for maxPause ticks without responding to player input
+  this.state = crashing;
+  this.maxPause = 5;
+  this.iPause = this.maxPause;
+  this.sprite = 'images/crash.png';
+  this.rowOffset = 50;
+};
+
+Player.prototype.setSplashing = function () {
+  // When player state is "splashing", change image and pause for maxPause ticks without responding to player input
+  this.state = splashing;
+  this.maxPause = 5;
+  this.iPause = this.maxPause;
+  this.sprite = 'images/splash.png';
+  this.rowOffset = 60;
+};
+
+// Pause while crashing or splashing and restart player when done
+// Parameter: dt, a time delta between ticks
+Player.prototype.pauseAndRestart = function (dt) {
+  if (this.iPause > 0) {
+    // Keep counting down to finish pausing
+    this.iPause = this.iPause - 10.0 * dt;
+  } else {
+    // Done pausing; reset player image and restart
+    this.setMoving();
+    this.move(2, 5);
   }
 };
 
 // Handle player input by updating game square col or row
-// Player x and y position will be calculated by 'update'
 Player.prototype.handleInput = function (playerInput) {
-  var numRows = 6;
-  var numCols = 5;
+  // Don't process player inputs while crashing or splashing
   if (player.state !== moving) {
-    // Don't process player inputs while pausing
     return;
   }
   switch (playerInput) {
   case 'up':
-    console.log("Player input: up");
     if (this.row > 0) {
       this.row--;
     };
     break;
   case 'down':
-    console.log("Player input: down");
     if (this.row < numRows - 1) {
       this.row++;
     };
     break;
   case 'left':
-    console.log("Player input: left");
     if (this.col > 0) {
       this.col--;
     };
     break;
   case 'right':
-    console.log("Player input: right");
     if (this.col < numCols - 1) {
       this.col++;
     };
     break;
   default:
-    console.log("Player input: something else");
   }
+  // Update player sprite on the canvas
   this.move();
-}
+};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
@@ -215,4 +226,4 @@ document.addEventListener('keyup', function (e) {
   };
 
   player.handleInput(allowedKeys[e.keyCode]);
-});
+});;
