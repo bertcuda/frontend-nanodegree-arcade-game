7a7f2d8b327ea78c96c701ad1b990311ed73cd13
Bert Ciccone <bertfciccone@gmail.com>
2016-10-11 19:46:31 -0700

    refactor: complete State pattern for player state

--- js/app.js
+++ js/app.js
@@ -9,7 +9,18 @@ var game = {
 };
 
 //
-// Shape - superclass
+// Player character sprites
+//
+var chars = [
+  'images/char-boy.png',
+  'images/char-cat-girl.png',
+  'images/char-horn-girl.png',
+  'images/char-pink-girl.png',
+  'images/char-princess-girl.png'
+];
+
+//
+// Shape - superclass for player and enemy
 //
 function Shape() {
   // position of shape on game grid
@@ -38,6 +49,9 @@ Shape.prototype.move = function (col, row) {
 Shape.prototype.render = function () {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
+
+// Enemy states
+var moving = "moving";
 
 // Enemy - subclass of Shape
 // Enemies our player must avoid
@@ -69,7 +83,7 @@ Enemy.prototype.update = function (dt) {
   // which will ensure the game runs at the same speed for
   // all computers.
 
-  // Update col position; after moving off the row, cycle back
+  // Update col position; after moving off the right side, cycle back
   var newCol = this.col + this.speed * dt;
   if (newCol >= game.numCols) {
     newCol = -1;
@@ -79,7 +93,7 @@ Enemy.prototype.update = function (dt) {
   this.move(newCol);
 
   // Check for collision with player
-  if (player.state === moving &&
+  if (player.state === playerState.moving &&
     player.row === this.row &&
     Math.abs(player.col - this.col) < 0.7) {
     // Player has just crashed; start crashing
@@ -104,21 +118,12 @@ function Player() {
   // Variables applied to each of our instances go here,
   // we've provided one for you to get started
   // Player sprite images
-  var chars = [
-    'images/char-boy.png',
-    'images/char-cat-girl.png',
-    'images/char-horn-girl.png',
-    'images/char-pink-girl.png',
-    'images/char-princess-girl.png',
-  ];
-  this.crashTime = 5;
-  this.splashTime = 5;
-  this.char = chars[0];
-  this.state = '';
-  this.timer = 0;
+  this.char = 0;
+  this.state = undefined;
+  // this.timer = 0;
   this.sprite = '';
   this.rowOffset = 0;
-  this.setMoving();
+  this.setSelecting();
 }
 // Player subclass extends Shape superclass
 Player.prototype = Object.create(Shape.prototype);
@@ -131,54 +136,70 @@ Player.prototype.constructor = Player;
 // Update the player's position, required method for game
 // Parameter: dt, a time delta between ticks
 Player.prototype.update = function (dt) {
-  // Check for win when player is at row 0 at the water
-  if (this.state === moving) {
-    if (this.row <= 0) {
-      // Player has just won; start splashing
-      this.setSplashing();
-      this.move();
-    }
-  } else {
-    // Stay in alternate state until timer expires
-    if (this.chargeTime(dt) <= 0) {
-      this.setMoving();
-      this.move(2, 5);
-    }
-  }
+  // Let the player state subclass take care of updates, if needed
+  this.state.update(dt);
 };
 
-// Character states
-var moving = "moving";
-var crashing = "crashing";
-var splashing = "splashing";
+// Handle player input by updating game square col or row
+Player.prototype.handleInput = function (playerInput) {
+  // Let the player state subclass take care of player input, if needed
+  this.state.handleInput(playerInput);
+};
+
+//
+// State transition methods
+//
+
+Player.prototype.setSelecting = function () {
+  // Player starts cycling through character sprite images to choose one
+  this.state = playerState.selecting;
+  this.sprite = chars[this.char];
+  this.rowOffset = -10;
+};
 
 Player.prototype.setMoving = function () {
-  // When player state is "moving", respond to player input
-  this.state = moving;
-  this.timer = 0;
-  this.sprite = this.char;
+  // Player starts moving according to input from the arrow keys
+  this.state = playerState.moving;
+  this.sprite = chars[this.char];
   this.rowOffset = -10;
 };
 
 Player.prototype.setCrashing = function () {
-  // When player state is "crashing", change image for crashTime ticks without responding to player input
-  this.state = crashing;
-  this.timer = this.crashTime;
+  // While player is "crashing", change player image for crashTime ticks without responding to player input
+  var crashTime = 5;
+  this.state = playerState.crashing;
+  this.state.timer = crashTime;
   this.sprite = 'images/crash.png';
   this.rowOffset = 50;
 };
 
 Player.prototype.setSplashing = function () {
-  // When player state is "splashing", change image for crashTime ticks without responding to player input
-  this.state = splashing;
-  this.timer = this.splashTime;
+  // While player is "splashing", change player image for splashTime ticks without responding to player input
+  var splashTime = 5;
+  this.state = playerState.splashing;
+  this.state.timer = splashTime;
   this.sprite = 'images/splash.png';
   this.rowOffset = 60;
 };
 
-// Deduct time and return the time remaining on the timer
+//
+// Player states using State design pattern
+// PlayerState - superclass
+//
+function PlayerState() {
+  // All state data is included in subclasses
+}
+
+// Update the player's position, required method for game
 // Parameter: dt, a time delta between ticks
-Player.prototype.chargeTime = function (dt) {
+PlayerState.prototype.update = function (dt) {
+  // no-op unless player state subclass provides it
+};
+
+// Deduct time and return the time remaining on the timer
+// Useful to track time in state initiated by an event (crashing or splashing)
+// Parameter: dt, a time delta between ticks
+PlayerState.prototype.chargeTime = function (dt) {
   if (this.timer > 0) {
     // Keep counting down
     this.timer = this.timer - 10.0 * dt;
@@ -186,42 +207,139 @@ Player.prototype.chargeTime = function (dt) {
   return this.timer;
 };
 
-// Handle player input by updating game square col or row
-Player.prototype.handleInput = function (playerInput) {
-  // Don't process player inputs while crashing or splashing
-  if (player.state !== moving) {
-    return;
-  }
+// Handle player input
+PlayerState.prototype.handleInput = function (playerInput) {
+  // no-op unless player state subclass provides it
+};
+
+//
+// PlayerSelecting - subclass of PlayerState
+// Player is in this state while selecting a character sprite
+//
+function PlayerSelecting() {
+  PlayerState.call(this);
+}
+
+// PlayerSelecting subclass extends PlayerState superclass
+PlayerSelecting.prototype = Object.create(PlayerState.prototype);
+PlayerSelecting.prototype.constructor = PlayerSelecting;
+
+// Up/down, left/right to cycle through character sprites, Space to start game
+PlayerSelecting.prototype.handleInput = function (playerInput) {
   switch (playerInput) {
   case 'up':
-    if (this.row > 0) {
-      this.row--;
-    };
+  case 'left':
+    player.char = player.char > 0 ? player.char - 1 : chars.length - 1;
+    player.sprite = chars[player.char];
     break;
   case 'down':
-    if (this.row < game.numRows - 1) {
-      this.row++;
-    };
-    break;
-  case 'left':
-    if (this.col > 0) {
-      this.col--;
-    };
-    break;
   case 'right':
-    if (this.col < game.numCols - 1) {
-      this.col++;
-    };
+    player.char = (player.char + 1) % chars.length;
+    player.sprite = chars[player.char];
+    break;
+  case 'space':
+    player.setMoving();
     break;
   default:
   }
   // Update player sprite on the canvas
-  this.move();
+  player.move();
+};
+
+//
+// PlayerMoving - subclass of PlayerState
+// Player is in this state while moving around in the game
+//
+function PlayerMoving() {
+  PlayerState.call(this);
+}
+
+// PlayerMoving subclass extends PlayerState superclass
+PlayerMoving.prototype = Object.create(PlayerState.prototype);
+PlayerMoving.prototype.constructor = PlayerMoving;
+
+PlayerMoving.prototype.update = function (dt) {
+  // Check for win when player is at row 0 at the water
+  if (player.row <= 0) {
+    // Player has just won; start splashing
+    player.setSplashing();
+    player.move();
+  }
+};
+
+// Handle player input by updating game square col or row
+PlayerMoving.prototype.handleInput = function (playerInput) {
+  switch (playerInput) {
+  case 'up':
+    player.row = player.row > 0 ? player.row - 1 : player.row;
+    break;
+  case 'down':
+    player.row = player.row < game.numRows - 1 ? player.row + 1 : player.row;
+    break;
+  case 'left':
+    player.col = player.col > 0 ? player.col - 1 : player.col;
+    break;
+  case 'right':
+    player.col = player.col < game.numCols - 1 ? player.col + 1 : player.col;
+    break;
+  default:
+  }
+  // Update player sprite on the canvas
+  player.move();
+};
+
+//
+// PlayerCrashing - subclass of PlayerState
+// Player is in this state while displaying the "crashing" graphic after colliding with an enemy
+//
+function PlayerCrashing() {
+  PlayerState.call(this);
+  this.timer = 0;
+}
+
+// PlayerCrashing subclass extends PlayerState superclass
+PlayerCrashing.prototype = Object.create(PlayerState.prototype);
+PlayerCrashing.prototype.constructor = PlayerCrashing;
+
+PlayerCrashing.prototype.update = function (dt) {
+  // Stay in crashing state until timer expires and then restart player
+  if (player.state.chargeTime(dt) <= 0) {
+    player.setMoving();
+    player.move(2, 5);
+  }
+};
+
+//
+// PlayerSplashing - subclass of PlayerState
+// Player is in this state while displaying the "splashing" graphic after reaching the water
+//
+function PlayerSplashing() {
+  PlayerState.call(this);
+  this.timer = 0;
+}
+
+// PlayerSplashing subclass extends PlayerState superclass
+PlayerSplashing.prototype = Object.create(PlayerState.prototype);
+PlayerSplashing.prototype.constructor = PlayerSplashing;
+
+PlayerSplashing.prototype.update = function (dt) {
+  // Stay in splashing state until timer expires and then restart player
+  if (player.state.chargeTime(dt) <= 0) {
+    player.setMoving();
+    player.move(2, 5);
+  }
 };
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+
+var playerState = {
+  "selecting": new PlayerSelecting(),
+  "moving": new PlayerMoving(),
+  "crashing": new PlayerCrashing(),
+  "splashing": new PlayerSplashing()
+}
 
 var allEnemies = [new Enemy(), new Enemy(), new Enemy()];
 allEnemies[0].move(-1, 1);
@@ -253,7 +371,8 @@ document.addEventListener('keyup', function (e) {
     37: 'left',
     38: 'up',
     39: 'right',
-    40: 'down'
+    40: 'down',
+    32: 'space'
   };
 
   player.handleInput(allowedKeys[e.keyCode]);
